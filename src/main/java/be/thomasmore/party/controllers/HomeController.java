86f87a05cb6c332cebf9ffb13f779b850cb32e03
Ilya Van der Hasselt <ilya.vanderhasselt@student.thomasmore.be>
2025-01-28 10:35:21 +0100

    variables toepassen

--- src/main/java/be/thomasmore/party/controllers/HomeController.java
+++ src/main/java/be/thomasmore/party/controllers/HomeController.java
@@ -1,16 +1,27 @@
 package be.thomasmore.party.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     @GetMapping({"/","/home"})
-    public String home(){
+    public String home(Model model) {
+        int myCalculatedValue = 34 * 62;
+        model.addAttribute("myCalculatedValue", myCalculatedValue);
         return "home";
     }
     @GetMapping("/about")
-    public String about(){
+    public String about(Model model) {
+        //myName and myCity,
+        //myStreet
+        String myName = "Ilya Van der Hasselt";
+        String myCity = "Mechelen";
+        String myStreet = "Oude Antwerpsebaan 155";
+        model.addAttribute("myName", myName);
+        model.addAttribute("myCity", myCity);
+        model.addAttribute("myStreet", myStreet);
         return "about";
     }
 
