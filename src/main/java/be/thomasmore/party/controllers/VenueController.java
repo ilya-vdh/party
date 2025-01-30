887f2999dacec832da3fd8212a86fc06e665a93a
Ilya Van der Hasselt <ilya.vanderhasselt@student.thomasmore.be>
2025-01-30 10:39:45 +0100

    database verfijning

--- src/main/java/be/thomasmore/party/controllers/VenueController.java
+++ src/main/java/be/thomasmore/party/controllers/VenueController.java
@@ -1,20 +1,22 @@
 package be.thomasmore.party.controllers;
 
+import be.thomasmore.party.repositories.VenueRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import be.thomasmore.party.model.Venue;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Optional;
+
 @Controller
 public class VenueController {
+    @Autowired
+    private VenueRepository venueRepository;
     @GetMapping("/venuedetails")
     public String venue(Model model) {
-        Venue venue = new Venue();
-        venue.setVenueName("sportpaleis");
-        venue.setLinkMoreInfo("https://www.sportpaleis.be/en/calendar?venue=AS");
-        model.addAttribute("venue", venue);
-        model.addAttribute("venueName", venue.getVenueName());
-        model.addAttribute("linkMoreInfo", venue.getLinkMoreInfo());
+        Optional<Venue> venueFromDb = venueRepository.findById(1);
+        venueFromDb.ifPresent(venue -> model.addAttribute("venue", venue));
         return "venuedetails";
     }
 }
---
+++ src/main/java/be/thomasmore/party/repositories/VenueRepository.java
@@ -0,0 +1,9 @@
+package be.thomasmore.party.repositories;
+
+
+import be.thomasmore.party.model.Venue;
+import org.springframework.data.repository.CrudRepository;
+
+public interface VenueRepository extends CrudRepository<Venue, Integer> {
+
+}
