eb06f347282ea6f10dd3bef70d42c5238a899bf9
Ilya Van der Hasselt <ilya.vanderhasselt@student.thomasmore.be>
2025-01-30 12:33:10 +0100

    nieuwe venue attributen toepassen

--- src/main/java/be/thomasmore/party/model/Venue.java
+++ src/main/java/be/thomasmore/party/model/Venue.java
@@ -12,6 +12,13 @@ public class Venue {
 
     private String venueName;
     private String linkMoreInfo;
+    private int capacity;
+    private boolean foodProvided;
+    private boolean indoor;
+    private boolean outdoor;
+    private boolean freeParkingAvailable;
+    private String city;
+    private int distanceFromPublicTransportInKm;
 
     public String getVenueName() {
         return venueName;
@@ -37,4 +44,59 @@ public class Venue {
         this.id = id;
     }
 
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public boolean isFoodProvided() {
+        return foodProvided;
+    }
+
+    public void setFoodProvided(boolean foodProvided) {
+        this.foodProvided = foodProvided;
+    }
+
+    public boolean isIndoor() {
+        return indoor;
+    }
+
+    public void setIndoor(boolean indoor) {
+        this.indoor = indoor;
+    }
+
+    public boolean isOutdoor() {
+        return outdoor;
+    }
+
+    public void setOutdoor(boolean outdoor) {
+        this.outdoor = outdoor;
+    }
+
+    public boolean isFreeParkingAvailable() {
+        return freeParkingAvailable;
+    }
+
+    public void setFreeParkingAvailable(boolean freeParkingAvailable) {
+        this.freeParkingAvailable = freeParkingAvailable;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getDistanceFromPublicTransportInKm() {
+        return distanceFromPublicTransportInKm;
+    }
+
+    public void setDistanceFromPublicTransportInKm(int distanceFromPublicTransportInKm) {
+        this.distanceFromPublicTransportInKm = distanceFromPublicTransportInKm;
+    }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,7 @@
-insert into venue (id, link_more_info, venue_name)
-values (1, 'https://www.sportpaleis.be/en/calendar?venue=AS', 'sportpaleis');
+INSERT INTO VENUE (ID, LINK_MORE_INFO, VENUE_NAME, CAPACITY,
+                   FOOD_PROVIDED, INDOOR, OUTDOOR, FREE_PARKING_AVAILABLE, CITY,
+                   DISTANCE_FROM_PUBLIC_TRANSPORT_IN_KM)
+values (1, 'https://www.sportpaleis.be/en/calendar?venue=AS',
+        'sportpaleis', 20, true, true,
+        false, false, 'Antwerpen',
+        15);
