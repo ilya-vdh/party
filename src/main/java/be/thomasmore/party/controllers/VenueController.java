3e5b183f7f3c63b2bfcc5919f666007ac5c59327
Ilya Van der Hasselt <ilya.vanderhasselt@student.thomasmore.be>
2025-02-11 11:42:12 +0100

    oefening 3.4

--- src/main/java/be/thomasmore/party/controllers/VenueController.java
+++ src/main/java/be/thomasmore/party/controllers/VenueController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import be.thomasmore.party.model.Venue;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Optional;
 
@@ -13,10 +14,28 @@ import java.util.Optional;
 public class VenueController {
     @Autowired
     private VenueRepository venueRepository;
-    @GetMapping("/venuedetails")
-    public String venue(Model model) {
-        Optional<Venue> venueFromDb = venueRepository.findById(1);
+
+    @GetMapping("/venuedetails/{id}")
+    public String venue(Model model, @PathVariable int id) {
+        Optional<Venue> venueFromDb = venueRepository.findById(id);
         venueFromDb.ifPresent(venue -> model.addAttribute("venue", venue));
         return "venuedetails";
+    }
+
+    @GetMapping("/zaaldetails")
+    public String venueList(Model model) {
+        Optional<Venue> venueFromDb1 = venueRepository.findById(1);
+        if (venueFromDb1.isPresent()) {
+            model.addAttribute("venue1", venueFromDb1.get());
+        }
+        Optional<Venue> venueFromDb2 = venueRepository.findById(2);
+        if (venueFromDb2.isPresent()) {
+            model.addAttribute("venue2", venueFromDb2.get());
+        }
+        Optional<Venue> venueFromDb3 = venueRepository.findById(3);
+        if (venueFromDb3.isPresent()) {
+            model.addAttribute("venue3", venueFromDb3.get());
+        }
+        return "zaaldetails";
     }
 }
